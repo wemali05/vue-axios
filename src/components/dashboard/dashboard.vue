e254e62c16286374c29e557fd80e1322b7881dde
wemali05 <alfredwemali05@gmail.com>
2020-02-20 08:54:01 +0300

    accessing and using response data

--- src/components/dashboard/dashboard.vue
+++ src/components/dashboard/dashboard.vue
@@ -2,15 +2,32 @@
   <div id="dashboard">
     <h1>That's the dashboard!</h1>
     <p>You should only get here if you're authenticated!</p>
+    <p>User email Address: {{ email }}</p>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
 export default {
+    data() {
+        return {
+            email:''
+        }
+    },
     created(){
         axios.get('https://vue-palylist-e6df4.firebaseio.com/users.json')
-        .then(res => console.log(res))
+        .then(res => {
+        console.log(res)
+        const data = res.data;
+        const users = [];
+        for(let key in data) {
+            const user = data[key];
+            user.id = key;
+            users.push(user)
+        }
+        console.log(users);
+        this.email = users[0].email;
+        })
         .catch(err =>console.log(err))
     }
 }
