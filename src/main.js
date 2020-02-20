273af054405e60418471cef0f58621f162cc3686
wemali05 <alfredwemali05@gmail.com>
2020-02-20 09:51:07 +0300

    use interceptors

--- src/main.js
+++ src/main.js
@@ -9,6 +9,18 @@ axios.defaults.baseURL = 'https://vue-palylist-e6df4.firebaseio.com'
 axios.defaults.headers.common['Authorization'] = 'afsafsafs'
 axios.defaults.headers.get['Accepts'] = 'Application/json'
 
+axios.interceptors.request.use(config => {
+    console.log('Request interceptor', config);
+    return config;
+})
+
+axios.interceptors.response.use(res => {
+    console.log('Response interceptor', res);
+    return res;
+})
+
+
+
 new Vue({
     el: '#app',
     router,
